944443514f49f91d1f40c5e7863489d89f08fa1e
Matias Seguel <mseguelestay@gmail.com>
2021-01-19 20:31:44 -0300

    Add User to Pet association using rails g migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_232214) do
+ActiveRecord::Schema.define(version: 2021_01_19_232532) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_01_19_232214) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "pets", "users"
 end
